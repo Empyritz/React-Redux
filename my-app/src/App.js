5fee75bc297f9af02dfae3acd296a128f3922483
Fabian NP <fabian.nava.pereda@gmail.com>
2022-09-29 12:17:38 -0500

    Using the configureStore with redox toolkit to make a efficient page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
 import {useSelector, useDispatch} from "react-redux"
+import { actions } from "./store/index"
 
 function App() {
   const counter = useSelector(state => state.counter)
   const dispatch = useDispatch()
   const increment = () => {
-    dispatch({ type: "INC" })
+    dispatch(actions.increment())
   }
   const decrement = () => {
-    dispatch({ type: "DEC" })
+    dispatch(actions.decrement()) 
   }
   const addBy = () => {
-    dispatch({ type: "ADD", payload: 10 })
+    dispatch(actions.addBy(10))
   }
+
+
+  //--------------------EXAMPLE WITH --------------------------
+  //<--the store before with the actions passed in an object-->
+  
+  // const addBy = () => {
+  //   dispatch({ type: "ADD", payload: 10 })
+  // }
   return (
     <div >
       <h1>Counter App</h1>
--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,22 +1,52 @@
-import { legacy_createStore as createStore } from "redux"
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+// import { configure } from "@testing-library/react";
 
-const reducerFn = (state={ counter: 20 }, action) => {
-    //Synchronus Function 
-    //We should not mutate the orginal state 
-
-    if(action.type === "INC"){
-        return { counter: state.counter + 1 }
+const counterSlice = createSlice({
+    name: "counter", 
+    initialState: { counter: 0 },
+    reducers: {
+        increment(state, action) {
+            state.counter ++
+        },
+        decrement(state, action) {
+            state.counter --
+        },
+        addBy(state, action) {
+            state.counter += action.payload
+        }
     }
-    if(action.type === "DEC"){
-        return { counter: state.counter - 1 }
-    }
-    if(action.type === "ADD"){
-        return { counter: state.counter + action.payload }
-    }
-
-    return state;
-}
-
-const store = createStore(reducerFn)
-
+})
+export const actions = counterSlice.actions
+const store = configureStore({
+    reducer: counterSlice.reducer 
+})
 export default store;
+
+
+
+
+// -------------------- FIRST STORE -----------------------
+
+
+// import { legacy_createStore as createStore } from "redux"
+
+// const reducerFn = (state={ counter: 20 }, action) => {
+//     //Synchronus Function 
+//     //We should not mutate the orginal state 
+
+//     if(action.type === "INC"){
+//         return { counter: state.counter + 1 }
+//     }
+//     if(action.type === "DEC"){
+//         return { counter: state.counter - 1 }
+//     }
+//     if(action.type === "ADD"){
+//         return { counter: state.counter + action.payload }
+//     }
+
+//     return state;
+// }
+
+// const store = createStore(reducerFn)
+
+// export default store;
